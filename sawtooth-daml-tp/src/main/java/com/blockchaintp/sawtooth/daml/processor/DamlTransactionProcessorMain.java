4466c41bb6c4dbc0c4f8f6a8b32f6559d9bc9062
Kevin O'Donnell <kevin@blockchaintp.com>
2019-05-03 00:48:41 -0400

    Add TransactionProcessorMain class
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

---
+++ sawtooth-daml-tp/src/main/java/com/blockchaintp/sawtooth/daml/processor/DamlTransactionProcessorMain.java
@@ -0,0 +1,31 @@
+package com.blockchaintp.sawtooth.daml.processor;
+
+import java.util.logging.Logger;
+
+import com.blockchaintp.sawtooth.daml.processor.impl.DamlCommitterImpl;
+import com.blockchaintp.sawtooth.daml.processor.impl.DamlTransactionHandler;
+import com.digitalasset.daml.lf.engine.Engine;
+
+import sawtooth.sdk.processor.TransactionHandler;
+import sawtooth.sdk.processor.TransactionProcessor;
+
+public class DamlTransactionProcessorMain {
+
+  private static final Logger LOGGER = Logger.getLogger(DamlTransactionProcessorMain.class.getName());
+
+  public static void main(String[] args) {
+    TransactionProcessor transactionProcessor = new TransactionProcessor(args[0]);
+    Engine engine = new Engine();
+    DamlCommitter committer = new DamlCommitterImpl(engine);
+    TransactionHandler handler = new DamlTransactionHandler(committer);
+    transactionProcessor.addHandler(handler);
+    Thread thread = new Thread(transactionProcessor);
+    thread.start();
+    try {
+      thread.join();
+    } catch (InterruptedException exc) {
+      LOGGER.warning("TransactionProcessor was interrupted");
+    }
+  }
+
+}
