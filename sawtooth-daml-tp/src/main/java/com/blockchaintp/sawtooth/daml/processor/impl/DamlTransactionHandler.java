9076ffa2b8e6e44e2b8e86187dea960441b6b19e
Kevin O'Donnell <kevin@blockchaintp.com>
2019-05-08 21:15:26 -0400

    Remove unused import
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- sawtooth-daml-tp/src/main/java/com/blockchaintp/sawtooth/daml/processor/impl/DamlTransactionHandler.java
+++ sawtooth-daml-tp/src/main/java/com/blockchaintp/sawtooth/daml/processor/impl/DamlTransactionHandler.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.UUID;
 
 import com.blockchaintp.sawtooth.daml.processor.DamlCommitter;
 import com.blockchaintp.sawtooth.daml.processor.LedgerState;
