c925073b7129e955dbc77f9c77b51692f535f322
Kevin O'Donnell <kevin@blockchaintp.com>
2020-04-06 22:00:50 -0400

    Extend LedgerState with LedgerStateOperations
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- sawtooth-daml-tp/src/main/java/com/blockchaintp/sawtooth/daml/processor/LedgerState.java
+++ sawtooth-daml-tp/src/main/java/com/blockchaintp/sawtooth/daml/processor/LedgerState.java
@@ -1,14 +1,12 @@
-/* Copyright 2019 Blockchain Technology Partners
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-------------------------------------------------------------------------------*/
+/*
+ * Copyright 2019 Blockchain Technology Partners Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
+ * law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+ * ------------------------------------------------------------------------------
+ */
 package com.blockchaintp.sawtooth.daml.processor;
 
 import java.util.Collection;
@@ -20,6 +18,7 @@ import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlLogEntry;
 import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlLogEntryId;
 import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlStateKey;
 import com.daml.ledger.participant.state.kvutils.DamlKvutils.DamlStateValue;
+import com.daml.ledger.validator.LedgerStateOperations;
 import com.google.protobuf.Timestamp;
 
 import sawtooth.sdk.processor.exceptions.InternalError;
@@ -30,16 +29,14 @@ import sawtooth.sdk.processor.exceptions.InvalidTransactionException;
  *
  * @author scealiontach
  */
-public interface LedgerState {
+public interface LedgerState extends LedgerStateOperations<LogResult> {
   /**
    * Fetch a single DamlStateValue from the ledger state.
    *
    * @param key DamlStateKey identifying the operation
    * @return the DamlStateValue for this key or null
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
   DamlStateValue getDamlState(DamlStateKey key) throws InternalError, InvalidTransactionException;
 
@@ -48,10 +45,8 @@ public interface LedgerState {
    *
    * @param keys a collection DamlStateKeys identifying the object
    * @return a map of DamlStateKey to DamlStateValue
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
   Map<DamlStateKey, DamlStateValue> getDamlStates(Collection<DamlStateKey> keys)
       throws InternalError, InvalidTransactionException;
@@ -61,10 +56,8 @@ public interface LedgerState {
    *
    * @param keys one or more DamlStateKeys identifying the values to be fetches
    * @return a map of DamlStateKeys to DamlStateValues
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
   Map<DamlStateKey, DamlStateValue> getDamlStates(DamlStateKey... keys)
       throws InternalError, InvalidTransactionException;
@@ -72,34 +65,28 @@ public interface LedgerState {
   /**
    * @param key The key identifying this DamlStateValue
    * @param val the DamlStateValue
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
-  void setDamlState(DamlStateKey key, DamlStateValue val) throws InternalError, InvalidTransactionException;
+  void setDamlState(DamlStateKey key, DamlStateValue val)
+      throws InternalError, InvalidTransactionException;
 
   /**
    * Store a collection of DamlStateValues at the logical keys provided.
    *
-   * @param entries a collection of tuples of DamlStateKey to DamlStateValue
-   *                mappings
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @param entries a collection of tuples of DamlStateKey to DamlStateValue mappings
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
   void setDamlStates(Collection<Entry<DamlStateKey, DamlStateValue>> entries)
       throws InternalError, InvalidTransactionException;
 
   /**
-   * @param entryId          Id of this log entry
-   * @param entry            the log entry to set
+   * @param entryId Id of this log entry
+   * @param entry   the log entry to set
    * @return the new entry list after the addition
-   * @throws InvalidTransactionException when there is an error relating to the
-   *                                     client input
-   * @throws InternalError               when there is an unexpected back end
-   *                                     error.
+   * @throws InvalidTransactionException when there is an error relating to the client input
+   * @throws InternalError               when there is an unexpected back end error.
    */
   List<String> addDamlLogEntry(DamlLogEntryId entryId, DamlLogEntry entry)
       throws InternalError, InvalidTransactionException;
