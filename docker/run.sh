b2fd01d09dba2d31227a7cd892289d859ed195bb
Ricardo Amaro <rjmAmaro@users.noreply.github.com>
2021-03-19 11:53:22 +0000

    Correct docker/run.sh to deal with paths with empty spaces
    When launching the Docker containers at a path
    with empty spaces, Docker was not able to correctly
    understand the path and the containers were not started.
    
    Correct the way the path is returned, so Docker can correctly
    understand it.
    
    Signed-off-by: Ricardo Amaro <rjmAmaro@users.noreply.github.com>

--- docker/run.sh
+++ docker/run.sh
@@ -3,17 +3,17 @@
 COMMAND=$1
 
 function start(){
-    if [ -d ${PWD}/docker/keys ]; then
-        rm -rf ${PWD}/docker/keys
+    if [ -d "${PWD}/docker/keys" ]; then
+        rm -rf "${PWD}/docker/keys"
     fi
-    docker-compose -f ${PWD}/docker/compose/daml-local.yaml up
+    docker-compose -f "${PWD}/docker/compose/daml-local.yaml" up
 }
 
 function stop(){
-    if [ -d ${PWD}/docker/keys ]; then
-        rm -rf ${PWD}/docker/keys
+    if [ -d "${PWD}/docker/keys" ]; then
+        rm -rf "${PWD}/docker/keys"
     fi
-    docker-compose -f ${PWD}/docker/compose/daml-local.yaml down
+    docker-compose -f "${PWD}/docker/compose/daml-local.yaml" down
 }
 
 case "$COMMAND" in
